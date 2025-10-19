f59435129b71951535b5a32c1fe52d9205dfae9c
Mitchell Davis <davismit@oregonstate.edu>
2025-10-19 17:02:52 -0500

    starting with amex prefix boundaries

--- tests.py
+++ tests.py
@@ -9,9 +9,18 @@ class TestCCValidator(unittest.TestCase):
 		self.assertFalse(credit_card_validator(card_num), msg='credit_card_validator({})'.format(card_num),)
 	
 	def test2(self):
-		"""Second test with an arbitrary valid number
-		Picked to test a valid number in gradescope, should return true"""
+		"""Boundary test with Amex prefix edge case of 33 (invalid)"""
 		card_num = "3312312312312315"
+		self.assertFalse(credit_card_validator(card_num), msg='credit_card_validator({})'.format(card_num),)
+
+	def test3(self):
+		"""Boundary test with Amex prefix edge case of 38 (invalid)"""
+		card_num = "381235769370231"
+		self.assertFalse(credit_card_validator(card_num), msg='credit_card_validator({})'.format(card_num),)
+
+	def test4(self):
+		"""Boundary test with Amex prefix edge case of 34 (valid)"""
+		card_num = "349478209483922"
 		self.assertTrue(credit_card_validator(card_num), msg='credit_card_validator({})'.format(card_num),)
 
 if __name__ == '__main__':
