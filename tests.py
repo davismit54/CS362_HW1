5c97dc40accecb178579cf5d1bb490f037475be4
Mitchell Davis <davismit@oregonstate.edu>
2025-10-19 16:48:04 -0500

    "adding first test"

--- tests.py
+++ tests.py
@@ -1,5 +1,12 @@
-def main():
-	pass
+import unittest
+from credit_card_validator import credit_card_validator
+
+class TestCCValidator(unittest.TestCase):
+	def test1(self):
+		"""First test with an arbitrary number.
+		Picked just to test my gradescope, but will fail length, prefix, and checksum"""
+		card_num = "0123456789"
+		self.assertFalse(credit_card_validator(card_num), msg='credit_card_validator({})'.format(card_num),)
 
 if __name__ == '__main__':
-	unittest.main()
+	unittest.main(verbosity=2)
